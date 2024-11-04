c691993b46b71dfbf552580a3474ec287a14054f
Adrien Pinel <156209614+Adriencalvados@users.noreply.github.com>
2024-11-04 11:16:18 +0100

    build_features make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,1 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+Nom du fichier : make_dataset.py
+Description : Ce script crée le fichier Config . local à partir du secret inclus dans git Récupère les datas..
+Auteur : Pinel.A
+Date : 2024-11-04
+"""
+
+import sys, subprocess,shutil,os,logging,inspect
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+# Fonctions
+def pull_data_with_dvc(logger):
+    logger = logging.getLogger(inspect.currentframe().f_code.co_name)
+    cmd = [sys.executable, "-m", "dvc", "pull"]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    if result.returncode == 0:
+        logger.info("extract OK")
+        logger.info(result.stdout)
+    else:
+        logger.info("extract NOK")
+        logger.info(result.stderr)
+    return result.returncode
+          
+def modifconfigsecret(file_path,secret,logger) :
+    logger = logging.getLogger(inspect.currentframe().f_code.co_name)
+    chemin_fichier = file_path+"config.local"
+
+    if os.path.exists(chemin_fichier):
+        logger.info("Le fichier existe.")
+    else:
+        logger.info("Le fichier n'existe pas.")
+        # Contenu du fichier
+        contenu = f"['remote \"origin\"']\n    access_key_id = {secret}\n    secret_access_key = {secret}"
+        # Création du fichier et écriture du contenu
+        with open(chemin_fichier, "w") as fichier:
+            result=fichier.write(contenu)
+            logger.info("credentials : ",result)
+        return result
+
+def import_dataset(logger,file_path, **kwargs):
+    logger = logging.getLogger(inspect.currentframe().f_code.co_name)
+    df_go = pd.read_csv(file_path+'/DATASET_GO_NG.csv', **kwargs)
+    df_go.target=-1
+    logger.info(f"df all nbre de lignes :{len(df_go)}")
+    df_ng= pd.read_csv(file_path+'/DATASET_NG.csv', **kwargs)
+    df_all=pd.concat([df_go,df_ng])
+    logger.info(f"df test nbre de lignes :{len(df_ng)}")
+    return df_all,df_ng
+    
+def split_data(df,df_ng):
+    # Split data into training and testing sets
+    target = df['target']
+    feats = df.drop(['target'], axis=1)
+    X_train, _, y_train, _ = train_test_split(feats, target, test_size=None, random_state=42)
+    y_test=df_ng['target']
+    X_test=df_ng.drop(['target'], axis=1)
+    return X_train, X_test, y_train, y_test
+    
+def create_folder_if_necessary(output_folderpath):
+    # Create folder if necessary
+    if os.path.exists(output_folderpath)==False:
+        os.makedirs(output_folderpath)
+
+def save_dataframes(X_train, X_test, y_train, y_test, output_folderpath):
+    # Save dataframes to their respective output file paths
+    for file, filename in zip([X_train, X_test, y_train, y_test], ['X_train', 'X_test', 'y_train', 'y_test']):
+        output_filepath = os.path.join(output_folderpath, f'{filename}.csv')
+        file.to_csv(output_filepath, index=False)
+        
+# Fonction principale
+def main(input_filepath='./data/raw', output_filepath='./data/interim'):
+    """
+    Point d'entrée principal du programme.
+    Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in../preprocessed).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('making data set from raw data')
+    # Récupérer les arguments de la ligne de commande
+    if len(sys.argv) > 1:
+        secret = str(sys.argv[1])  # Convertir le 1er argument en entier
+    else:
+        logger.info("Veuillez fournir un paramètre str en argument.")
+        sys.exit(1)
+
+    # Appel de fonction avec le paramètre d'entrée
+    resultat = modifconfigsecret(".dvc/",secret,logger)
+    logger.info(f"Le résultat est : {resultat}")
+    # Appel de fonction pull dvc
+    resultat = pull_data_with_dvc(logger)
+    logger.info(f"Le résultat est : {resultat}")
+
+    df,df_ng = import_dataset(logger,input_filepath, sep=';',decimal=',',index_col="index")
+    # Split data into training and testing sets    
+    X_train, X_test, y_train, y_test = split_data(df,df_ng)
+
+    # Create folder if necessary
+    create_folder_if_necessary(output_filepath)
+
+    # Save dataframes to their respective output file paths
+    logger.info('save_dataframes progress ...')
+    save_dataframes(X_train, X_test, y_train, y_test, output_filepath)
+    
+    logger.info('make interim data set')
+
+# Bloc pour lancer le script en tant que programme principal
+if __name__ == "__main__":
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    main()
---
+++ src/features/build_features.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import click
+import logging
+from sklearn.preprocessing import StandardScaler
+import os,inspect
+
+def main(input_filepath='./data/interim', output_filepath='./data/processed'):
+    """ Runs data processing scripts to turn raw data from (../raw) into
+        cleaned data ready to be analyzed (saved in../preprocessed).
+    """
+    global logger
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from interim')
+
+    fX_test = f"{input_filepath}/X_test.csv"
+    fX_train = f"{input_filepath}/X_train.csv"
+    
+    process_data(fX_test, fX_train, output_filepath)
+
+def process_data(fX_test, fX_train,output_filepath):
+    # Import datasets
+    logger = logging.getLogger(inspect.currentframe().f_code.co_name)
+    X_test = import_dataset(fX_test).drop(columns=["moyenne","ecartype","mediane","min","max"])
+    X_train = import_dataset(fX_train).drop(columns=["moyenne","ecartype","mediane","min","max"])
+    scaler = StandardScaler()
+    print(X_train.head(2))
+    
+    X_train_scaled=scaler.fit_transform(X_train)
+    X_test_scaled=scaler.transform(X_test)
+
+    # Create folder if necessary
+    create_folder_if_necessary(output_filepath)
+
+    # Save dataframes to their respective output file paths
+    save_dataframes(X_train_scaled, X_test_scaled, output_filepath)
+
+def import_dataset(file_path, **kwargs):    
+    logger = logging.getLogger(inspect.currentframe().f_code.co_name)
+    logger.info('Import ...')
+    return pd.read_csv(file_path, **kwargs)
+
+def create_folder_if_necessary(output_folderpath):
+    logger = logging.getLogger(inspect.currentframe().f_code.co_name)
+    logger.info('create_folder_if_necessary')
+    # Create folder if necessary
+    if os.path.exists(output_folderpath)==False:
+        os.makedirs(output_folderpath)
+
+def save_dataframes(X_train_scaled, X_test_scaled, output_folderpath):
+    logger = logging.getLogger(inspect.currentframe().f_code.co_name)
+    logger.info('save dataframes')
+    # Save dataframes to their respective output file paths
+    for file, filename in zip([X_train_scaled, X_test_scaled], ['X_train_scaled', 'X_test_scaled']):
+        output_filepath = os.path.join(output_folderpath, f'{filename}.csv')
+        #file.to_csv(output_filepath, index=False)
+        np.savetxt(output_filepath, file, delimiter=",")
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    main()
