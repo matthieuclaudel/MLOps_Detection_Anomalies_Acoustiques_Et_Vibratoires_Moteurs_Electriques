3f90f1da4f038a0e273f4302bc709259dff81dcf
Adrien Pinel <156209614+Adriencalvados@users.noreply.github.com>
2024-11-18 21:36:54 +0100

    workflows

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,13 +7,12 @@ Description : Ce script crée le fichier Config . local à partir du secret incl
 Auteur : Pinel.A
 Date : 2024-11-04
 """
-
 import sys, subprocess, os, logging, inspect
 import pandas as pd
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 # Fonctions
-def pull_data_with_dvc(logger) :
+def pull_data_with_dvc(logger):
     logger = logging.getLogger(inspect.currentframe().f_code.co_name)
     cmd = [sys.executable, "-m", "dvc", "pull"]
     result = subprocess.run(cmd, capture_output=True, text=True)
@@ -25,7 +24,7 @@ def pull_data_with_dvc(logger) :
         logger.info(result.stderr)
     return result.returncode
 
-def modifconfigsecret(file_path,secret,logger) :
+def modifconfigsecret(file_path, secret, logger):
     logger = logging.getLogger(inspect.currentframe().f_code.co_name)
     chemin_fichier = file_path+"config.local"
 
@@ -41,26 +40,26 @@ def modifconfigsecret(file_path,secret,logger) :
             logger.info("credentials pass")
         return result
 
-def import_dataset(logger,file_path, **kwargs) :
+def import_dataset(logger, file_path, **kwargs):
     logger = logging.getLogger(inspect.currentframe().f_code.co_name)
     df_go = pd.read_csv(file_path+'/DATASET_GO_NG.csv', **kwargs)
     logger.info(f"df all nbre de lignes :{len(df_go)}")
-    df_ng= pd.read_csv(file_path+'/DATASET_NG.csv', **kwargs)
+    df_ng = pd.read_csv(file_path+'/DATASET_NG.csv', **kwargs)
     logger.info(f"df test nbre de lignes :{len(df_ng)}")
-    return df_go,df_ng
+    return df_go, df_ng
     
-def split_data(df,df_ng):
+def split_data(df, df_ng):
     # Split data into training and testing sets
-    df_all=pd.concat([df,df_ng])
+    df_all = pd.concat([df, df_ng])
     X = shuffle(df_all)
 
     # Séparer les données sans NaN (clean) et celles avec NaN
     X_clean = X.dropna()
     X_with_nan = X[X.isna().any(axis=1)]
-    Y_clean=X_clean["target"].astype(float).astype(int)
+    Y_clean = X_clean["target"].astype(float).astype(int)
     # Split uniquement sur les données sans NaN avec stratification
     X_train_clean, X_test, y_train_clean, y_test = train_test_split(
-        X_clean.drop(["target"],axis=1), Y_clean, test_size=0.1, stratify=Y_clean, random_state=42
+        X_clean.drop(["target"], axis=1), Y_clean, test_size=0.1, stratify=Y_clean, random_state=42
     )
     # Ajouter les données contenant des NaN uniquement à X_train et y_train
     X_train = pd.concat([X_train_clean, X_with_nan.drop(["target"],axis=1)])
@@ -75,7 +74,7 @@ def split_data(df,df_ng):
     
 def create_folder_if_necessary(output_folderpath):
     # Create folder if necessary
-    if os.path.exists(output_folderpath)==False:
+    if os.path.exists(output_folderpath) == False:
         os.makedirs(output_folderpath)
 
 def save_dataframes(X_train, X_test, y_train, y_test, output_folderpath) :
@@ -101,15 +100,15 @@ def main(input_filepath='./data/raw', output_filepath='./data/interim') :
         sys.exit(1)
 
     # Appel de fonction avec le paramètre d'entrée
-    resultat = modifconfigsecret(".dvc/",secret,logger)
+    resultat = modifconfigsecret(".dvc/", secret,logger)
     logger.info(f"Le résultat est : {resultat}")
     # Appel de fonction pull dvc
     resultat = pull_data_with_dvc(logger)
     logger.info(f"Le résultat est : {resultat}")
 
-    df,df_ng = import_dataset(logger,input_filepath, sep=';',decimal=',',index_col="index")
+    df,df_ng = import_dataset(logger, input_filepath, sep=';', decimal=',', index_col="index")
     # Split data into training and testing sets    
-    X_train, X_test, y_train, y_test = split_data(df,df_ng)
+    X_train, X_test, y_train, y_test = split_data(df, df_ng)
 
     # Create folder if necessary
     create_folder_if_necessary(output_filepath)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Nom du fichier : train_model.py
+Description : 
+Auteur : Pinel.A
+Date : 2024-11-04
+"""
 import pickle
 import pandas as pd
 from sklearn.neighbors import LocalOutlierFactor
@@ -5,7 +14,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 def import_dataset(file_path, **kwargs):
     return pd.read_csv(file_path, **kwargs)
 
-input_filepath='./data/processed'
+input_filepath = './data/processed'
 fX_train = f"{input_filepath}/X_train_scaled.csv"
 f_ytrain = f"{input_filepath}/y_train.csv"
 fX_test = f"{input_filepath}/X_test_scaled.csv"
@@ -16,18 +25,18 @@ y_train = import_dataset(f_ytrain)
 
 # Instanciation de l'algo LOF de base
 params = {
-    "novelty":True,
-    "n_neighbors":2,
-    "contamination":0.25,
-    "metric":"cosine",
-    "n_jobs":-1,
+    "novelty": True,
+    "n_neighbors": 2,
+    "contamination": 0.25,
+    "metric": "cosine",
+    "n_jobs": -1,
 }
 model = LocalOutlierFactor(**params)
     # Training sans réduction de dimension
-y_pred=model.fit(X_train)
-y_pred=model.predict(X_train)
+y_pred = model.fit(X_train)
+y_pred = model.predict(X_train)
 y_pred[y_pred == 1] = 0
-print("Rapport de classification : \n",classification_report(y_train, y_pred),"\n")
+print("Rapport de classification : \n", classification_report(y_train, y_pred), "\n")
 print(confusion_matrix(y_train.values, y_pred=y_pred))
 # Sauvegarder le modèle entraîné dans un fichier .pkl
 model_filename = 'models/trained_LOF_model.pkl'
