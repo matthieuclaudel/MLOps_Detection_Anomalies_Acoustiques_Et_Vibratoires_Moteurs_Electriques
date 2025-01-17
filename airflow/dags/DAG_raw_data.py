90785128e49e1c83453de4a38a5bd07e3881d8ec
ludodo23 <149336350+ludodo23@users.noreply.github.com>
2025-01-17 22:14:39 +0100

    ajout mongo to csv DAG

---
+++ airflow/dags/DAG_raw_data.py
@@ -0,0 +1,54 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+from datetime import datetime
+import os
+
+# Configuration des volumes partagés et des chemins
+SHARED_DATA_PATH = "/shared_data"
+OUTPUT_FILE = f"{SHARED_DATA_PATH}/output.csv"
+
+# Volume partagé entre les pods
+shared_volume = k8s.V1Volume(
+    name='data-folder',
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='data-folder')
+)
+
+shared_volume_mount = k8s.V1VolumeMount(
+    name='data-folder',
+    mount_path=SHARED_DATA_PATH,
+)
+
+# Définir le DAG
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+}
+
+with DAG(
+    "mongo_to_csv",
+    default_args=default_args,
+    description="Read from MongoDB and write to CSV in a shared volume",
+    schedule_interval=None,
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+) as dag:
+
+    # Tâche : Lire les données depuis MongoDB et écrire dans un fichier CSV
+    mongo_to_csv_task = KubernetesPodOperator(
+        task_id="mongo_to_csv",
+        name="mongo-to-csv",
+        namespace="default",
+        image="ludodo/mlops-dst-project-get-from-mongo:latest",
+        env_vars={
+            "MONGO_URI": "mongodb://mongodb:27017",
+            "MONGO_DB": "production",
+            "MONGO_COLLECTION": "api_mesures",
+            "OUTPUT_FILE": OUTPUT_FILE,
+        },
+        volumes=[shared_volume],
+        volume_mounts=[shared_volume_mount],
+        get_logs=True,
+    )
