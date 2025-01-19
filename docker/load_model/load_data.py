61105e044695575507b12003e756a4d897576594
matthieuclaudel <crotelius77@gmail.com>
2025-01-19 16:05:38 +0100

    MAJ load_data.py avec source de données raw.csv et enregistrement du scaler

--- docker/load_model/load_data.py
+++ docker/load_model/load_data.py
@@ -1,13 +1,44 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import pickle
 
-data_train = pd.read_csv('https://raw.githubusercontent.com/matthieuclaudel/MLOps_Detection_Anomalies_Acoustiques_Et_Vibratoires_Moteurs_Electriques/refs/heads/master/data/raw/DATASET_LIGHT_SCALED.csv', sep=';', decimal=',', index_col=0)
+def load_data():
+    # chargement du jeu de données raw.csv généré par le script de prétraitement via le DAG_raw_data.py
+    data_train = pd.read_csv('/app/test/raw.csv', sep=',')
+    # suppression de la colonne date_de_creation
+    data_train = data_train.drop(columns=['date_de_creation'], axis=1)
 
-data_train.to_csv('/app/test/data_train.csv', index=False)
+    # Normalisation des données avec un standard scaler
+    scaler_model = StandardScaler()
+    raw_values_scaled = scaler_model.fit_transform(data_train)
+    data_train_scaled = pd.DataFrame(raw_values_scaled,)
+    data_train_scaled.columns = data_train.columns
 
-data_test = pd.read_csv('https://raw.githubusercontent.com/matthieuclaudel/MLOps_Detection_Anomalies_Acoustiques_Et_Vibratoires_Moteurs_Electriques/refs/heads/master/data/raw/DATASET_VALIDATION_SCALED.csv', sep=';', decimal=',', index_col=0)
+    # Chargement du jeu de données de validation et normalisation avec le modèle de normalisation
+    data_validation = pd.read_csv('/app/test/DATASET_VALIDATION.csv', sep=',') 
+    target = data_validation['target']
+    index = data_validation['index']
+    data_validation = data_validation.drop(columns=['index','target'], axis=1)
+    # Normalisation des données de validation
+    validation_values_scaled = scaler_model.transform(data_validation)
+    data_validation_scaled = pd.DataFrame(validation_values_scaled,)
+    data_validation_scaled.columns = data_validation.columns
+    data_validation_scaled['target'] = target.values
+    data_validation_scaled['index'] = index.values
 
-data_test.to_csv('/app/test/data_test.csv', index=False)
+    data_train_scaled.to_csv('/app/test/data_train.csv', index=False)
+    data_validation_scaled.to_csv('/app/test/data_test.csv', index=False)
+    
+    print('Data loaded successfully')
+
+def saved_scaler_model():   
+    # Sauvegarde du modèle de normalisation dans le répertoire /app/test
+    with open('/app/test/scaler_model.pkl', 'wb') as f:
+        pickle.dump(scaler_model, f)
 
 
-print(data_train.columns)
-print('Data loaded successfully')
+if __name__ == "__main__":
+    load_data()
+    saved_scaler_model()
+    
+    
